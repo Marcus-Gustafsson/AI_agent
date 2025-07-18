7e68e9cc774d41cb4c599dd09bb92c9fb6cd9b71
Marcus-Gustafsson <marcus.thomas.gustafsson@hotmail.com>
2025-07-18 07:48:38 +0200

    Added ability to custom prompt via terminal

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import os
+import os, sys
 from dotenv import load_dotenv
 from google import genai
 
@@ -10,10 +10,13 @@ client = genai.Client(api_key=api_key)
 
 
 def main():
-    print("Hello from ai-agent!")
+    # Ensure that the correct number of arguments is provided 
+    if len(sys.argv) < 2: 
+        print('Error (No prompt), use: uv run main.py "<insert prompt here>"') 
+        sys.exit(1) 
+    cmd_args = sys.argv
     response = client.models.generate_content(
-    model='gemini-2.0-flash-001', contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
-    )
+    model='gemini-2.0-flash-001', contents=cmd_args[1])
     print(response.text)
     print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
     print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
